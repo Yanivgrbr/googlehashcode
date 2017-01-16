bc239ecb2553e73c1939f68712de65fd2568fc93
Sharon Brizinov <SBSoftware@me.com>
2017-01-16 13:44:03 -0600

    - Check commit

---
+++ parser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+DEFAULT_INPUT_FILE_NAME = "asd.txt"
+
+def read_input_file(file_name = DEFAULT_INPUT_FILE_NAME):
+    '''
+    Example:
+        3 5 1 6
+        TTTTT
+        TMMMT
+        TTTTT
+    '''
+    pass
