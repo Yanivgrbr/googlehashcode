4c287327f6975d7e3f4045cf915afb5ae56515b2
Sharon Brizinov <SBSoftware@me.com>
2017-01-16 15:36:45 -0600

    - added is_valid function

--- src/Pizza.py
+++ src/Pizza.py
@@ -28,3 +28,42 @@ class Pizza(object):
 
     def get_ingredient(self, row, col):
         return self.__pizza_layout[row][col]
+
+    def is_valid(self, slice):
+        '''
+        - Doesn't exceed pizza's boundaries
+        - Doesn't exceed maximum number of cells
+        - Has minimum amount of tomatoes and mushrooms
+        '''
+
+        # Boundaries
+        if slice.left < 0 or slice.top < 0:
+            return False
+
+        if slice.right >= self.num_of_rows or slice.bottom >= self.num_of_rows:
+            return False
+
+        # Maximum # of cells
+        if slice.width * slice.height > self.max_cells_per_slice:
+            return False
+
+        # Enough mushrooms & tomatoes
+        number_of_tomatoes = 0
+        number_of_mushrooms = 0
+        for col in xrange(slice.width + 1):
+            for row in xrange(slice.height + 1):
+                ingredient = self.get_ingredient(self.top + row, slice.left + col)
+
+                if type(ingredient) == Mushroom:
+                    number_of_mushrooms += 1
+
+                if type(ingredient) == Tomato:
+                    number_of_tomatoes += 1
+
+
+        if number_of_tomatoes < self.min_ingredients or number_of_mushrooms < self.min_ingredients:
+            return False
+
+
+        # Nothing failed, all's good!
+        return True
